b2437e2f996e5fa687c3c8a0b05906d6217b8484
OmdaMukhtar <abuoop123@gmail.com>
2020-09-26 11:58:29 +0200

    implement component base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
 import React from "react";
 
-function App({title}){
-    return <div><h1>{title}</h1></div>
+// function App({title}){
+//     return <div><h1>{title}</h1></div>
+// }
+
+class App extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {title: props.title};
+    }
+
+    render() {
+        return <div><h1>{this.state.title}</h1></div>
+    }
 }
 
 export default App;
